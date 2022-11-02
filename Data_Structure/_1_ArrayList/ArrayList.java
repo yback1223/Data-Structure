9ffb6a355930ac350ed2b38d089543a9ddf07130
yback1223 <yoyob1223@gmail.com>
2022-11-02 14:28:58 +0900

    style: google java style

--- Data_Structure/_1_ArrayList/ArrayList.java
+++ Data_Structure/_1_ArrayList/ArrayList.java
@@ -1,18 +1,17 @@
 package _1_ArrayList;
 
-import my_interface.List;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import my_interface.MyList;
 
 
-public class ArrayList<E> implements List<E>, Cloneable, Iterable<E> {
+public class ArrayList<E> implements MyList<E>, Cloneable, Iterable<E> {
 
-     private static final int DEFAULT_CAPACITY = 10;
+  private static final int DEFAULT_CAPACITY = 10;
 
-     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+  private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
      /*
           The maximum length of array to allocate.
           확장 가능한 용적의 한계값
@@ -24,59 +23,60 @@ public class ArrayList<E> implements List<E>, Cloneable, Iterable<E> {
           제한 값의 초과를 방지해야 위의 에러가 뜨지 않으므로, 이론적으로 가능한 최댓값에 8을 뺀 값으로 최대 크기를 지정한다.
       */
 
-     private static final Object[] EMPTY_ARRAY = {};
-     private int size;
+  private static final Object[] EMPTY_ARRAY = {};
+  private int size;
 
-     Object[] array;
+  Object[] array;
 
-     public ArrayList() {
-          this.array = EMPTY_ARRAY;
-          this.size = 0;
-     }
+  public ArrayList() {
+    this.array = EMPTY_ARRAY;
+    this.size = 0;
+  }
 
-     public ArrayList(int capacity) {
-          if (capacity < 0)
-               throw new IllegalArgumentException();
+  public ArrayList(int capacity) {
+       if (capacity < 0) {
+            throw new IllegalArgumentException();
+       }
 
-          if (capacity == 0) {
-               array = EMPTY_ARRAY;
-          } else {
-               array = new Object[capacity];
-          }
-          this.size = 0;
-     }
+    if (capacity == 0) {
+      array = EMPTY_ARRAY;
+    } else {
+      array = new Object[capacity];
+    }
+    this.size = 0;
+  }
 
-     private void resize() {
-          int array_capacity = array.length;
+  private void resize() {
+    int array_capacity = array.length;
 
-          // if (array_capacity == 0)
-          if (Arrays.equals(array, EMPTY_ARRAY)) {
-               array = new Object[DEFAULT_CAPACITY];
-               return;
-          }
+    // if (array_capacity == 0)
+    if (Arrays.equals(array, EMPTY_ARRAY)) {
+      array = new Object[DEFAULT_CAPACITY];
+      return;
+    }
 
-          // if array is full
-          if (size == array_capacity) {
+    // if array is full
+    if (size == array_capacity) {
 
-               // default growing 1.5x
-               int new_capacity = hugeRangeCheck(array_capacity, array_capacity + (array_capacity << 1));
+      // default growing 1.5x
+      int new_capacity = hugeRangeCheck(array_capacity, array_capacity + (array_capacity << 1));
 
-               // copy
-               array = Arrays.copyOf(array, new_capacity);
-               return;
-          }
+      // copy
+      array = Arrays.copyOf(array, new_capacity);
+      return;
+    }
 
-          // if array is less than half full
-          if (size < (array_capacity >> 1)) {
-               int new_capacity = array_capacity >>> 1;
+    // if array is less than half full
+    if (size < (array_capacity >> 1)) {
+      int new_capacity = array_capacity >>> 1;
 
-               // copy
-               array = Arrays.copyOf(array, Math.max(new_capacity, DEFAULT_CAPACITY));
-               return;
-          }
-     }
+      // copy
+      array = Arrays.copyOf(array, Math.max(new_capacity, DEFAULT_CAPACITY));
+      return;
+    }
+  }
 
-     private int hugeRangeCheck(int oldCapacity, int newCapacity) {
+  private int hugeRangeCheck(int oldCapacity, int newCapacity) {
           /*
                resizing할 때, overflow를 방지하기 위한 체크 함수
                용적은 MAX_ARRAY_SIZE를 초과할 수 없다.
@@ -85,241 +85,241 @@ public class ArrayList<E> implements List<E>, Cloneable, Iterable<E> {
                @param newCapacity - resize하고자 하는 용적
                @return - 최종 크기 반환
            */
-          if (MAX_ARRAY_SIZE - size <= 0) {
-               throw new OutOfMemoryError("Required array length too large");
-          }
+    if (MAX_ARRAY_SIZE - size <= 0) {
+      throw new OutOfMemoryError("Required array length too large");
+    }
 
-          if (newCapacity >= 0) { // no overflow
-               if (newCapacity - MAX_ARRAY_SIZE <= 0) {
-                    return newCapacity;
-               }
-               return MAX_ARRAY_SIZE;
-          } else { // newCapacity is overflow
-               int fiveFourtheSize = oldCapacity + (oldCapacity >> 2);
+    if (newCapacity >= 0) { // no overflow
+      if (newCapacity - MAX_ARRAY_SIZE <= 0) {
+        return newCapacity;
+      }
+      return MAX_ARRAY_SIZE;
+    } else { // newCapacity is overflow
+      int fiveFourtheSize = oldCapacity + (oldCapacity >> 2);
 
-               if (fiveFourtheSize <= 0 || fiveFourtheSize >= MAX_ARRAY_SIZE) {
-                    return MAX_ARRAY_SIZE;
-               }
-               return fiveFourtheSize;
-          }
-     }
+      if (fiveFourtheSize <= 0 || fiveFourtheSize >= MAX_ARRAY_SIZE) {
+        return MAX_ARRAY_SIZE;
+      }
+      return fiveFourtheSize;
+    }
+  }
 
-     @Override
-     public boolean add(E value) {
-          addLast(value);
-          return true;
-     }
+  @Override
+  public boolean add(E value) {
+    addLast(value);
+    return true;
+  }
 
-     public void addLast(E value) {
-          if (size == array.length) {
-               resize();
-          }
-          array[size] = value;
-          size++;
-     }
+  public void addLast(E value) {
+    if (size == array.length) {
+      resize();
+    }
+    array[size] = value;
+    size++;
+  }
 
-     public void addFirst(E value) {
-          add(0, value);
-     }
+  public void addFirst(E value) {
+    add(0, value);
+  }
 
-     @Override
-     public void add(int index, E value) {
-          if (index > size || index < 0) {
-               throw new IndexOutOfBoundsException();
-          }
+  @Override
+  public void add(int index, E value) {
+    if (index > size || index < 0) {
+      throw new IndexOutOfBoundsException();
+    }
 
-          if (index == size) {
-               addLast(value);
-               return;
-          }
+    if (index == size) {
+      addLast(value);
+      return;
+    }
 
-          if (size == array.length) {
-               resize();
-          }
+    if (size == array.length) {
+      resize();
+    }
 
-          for (int i = size; i > index; i--) {
-               array[i] = array[i - 1];
-          }
-          array[index] = value;
-          size++;
-     }
+    for (int i = size; i > index; i--) {
+      array[i] = array[i - 1];
+    }
+    array[index] = value;
+    size++;
+  }
 
-     @SuppressWarnings("unchecked")
-     @Override
-     public E get(int index) {
-          if (index >= size || index < 0) {
-               throw new IndexOutOfBoundsException();
-          }
-          return (E) array[index];
-     }
+  @SuppressWarnings("unchecked")
+  @Override
+  public E get(int index) {
+    if (index >= size || index < 0) {
+      throw new IndexOutOfBoundsException();
+    }
+    return (E) array[index];
+  }
 
-     @Override
-     public void set(int index, E value) {
-          if (index >= size || index < 0) {
-               throw new IndexOutOfBoundsException();
-          } else {
-               array[index] = value;
-          }
-     }
+  @Override
+  public void set(int index, E value) {
+    if (index >= size || index < 0) {
+      throw new IndexOutOfBoundsException();
+    } else {
+      array[index] = value;
+    }
+  }
 
-     @Override
-     public int indexOf(Object value) {
-          if (value == null) {
-               for (int i = 0; i < size; i++) {
-                    if (array[i] == null) {
-                         return i;
-                    }
-               }
-          } else {
-               for (int i = 0; i < size; i++) {
-                    if (value.equals(array[i])) {
-                         return i;
-                    }
-               }
-          }
-          return -1;
-     }
+  @Override
+  public int indexOf(Object value) {
+    if (value == null) {
+      for (int i = 0; i < size; i++) {
+        if (array[i] == null) {
+          return i;
+        }
+      }
+    } else {
+      for (int i = 0; i < size; i++) {
+        if (value.equals(array[i])) {
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
 
-     public int lastIndexOf(Object value) {
-          if (value == null) {
-               for (int i = size - 1; i >= 0; i--) {
-                    if (array[i] == null) {
-                         return i;
-                    }
-               }
-          } else {
-               for (int i = size - 1; i >= 0; i--) {
-                    if (value.equals(array[i])) {
-                         return i;
-                    }
-               }
-          }
-          return -1;
-     }
+  public int lastIndexOf(Object value) {
+    if (value == null) {
+      for (int i = size - 1; i >= 0; i--) {
+        if (array[i] == null) {
+          return i;
+        }
+      }
+    } else {
+      for (int i = size - 1; i >= 0; i--) {
+        if (value.equals(array[i])) {
+          return i;
+        }
+      }
+    }
+    return -1;
+  }
 
-     @Override
-     public boolean contains(Object value) {
-          return indexOf(value) >= 0;
-     }
+  @Override
+  public boolean contains(Object value) {
+    return indexOf(value) >= 0;
+  }
 
-     @SuppressWarnings("unchecked")
-     @Override
-     public E remove(int index) {
-          if (index >= size || index < 0) {
-               throw new IndexOutOfBoundsException();
-          }
+  @SuppressWarnings("unchecked")
+  @Override
+  public E remove(int index) {
+    if (index >= size || index < 0) {
+      throw new IndexOutOfBoundsException();
+    }
 
-          E element = (E) array[index];
-          array[index] = null;
+    E element = (E) array[index];
+    array[index] = null;
 
-          for (int i = index; i < size - 1; i++) {
-               array[i] = array[i + 1];
-               array[i + 1] = null;
-          }
-          size--;
-          resize();
-          return element;
-     }
+    for (int i = index; i < size - 1; i++) {
+      array[i] = array[i + 1];
+      array[i + 1] = null;
+    }
+    size--;
+    resize();
+    return element;
+  }
 
-     @Override
-     public boolean remove(Object value) {
-          int index = indexOf(value);
-          if (index == -1) {
-               return false;
-          }
-          remove(value);
-          return true;
-     }
+  @Override
+  public boolean remove(Object value) {
+    int index = indexOf(value);
+    if (index == -1) {
+      return false;
+    }
+    remove(value);
+    return true;
+  }
 
-     @Override
-     public boolean isEmpty() {
-          return size == 0;
-     }
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
-     @Override
-     public int size() {
-          return size;
-     }
+  @Override
+  public int size() {
+    return size;
+  }
 
-     @Override
-     public void clear() {
-          for (int i = 0; i < size; i++) {
-               array[i] = null;
-          }
-          size = 0;
-          resize();
-     }
+  @Override
+  public void clear() {
+    for (int i = 0; i < size; i++) {
+      array[i] = null;
+    }
+    size = 0;
+    resize();
+  }
 
-     @Override
-     public Object clone() {
+  @Override
+  public Object clone() {
 
-          try {
-               ArrayList<?> cloneList = (ArrayList<?>) super.clone();
-               cloneList.array = new Object[size];
+    try {
+      ArrayList<?> cloneList = (ArrayList<?>) super.clone();
+      cloneList.array = new Object[size];
 
-               System.arraycopy(array, 0, cloneList.array, 0, size);
+      System.arraycopy(array, 0, cloneList.array, 0, size);
 
-               return cloneList;
-          } catch (CloneNotSupportedException e) {
-               throw new Error(e);
-          }
-     }
+      return cloneList;
+    } catch (CloneNotSupportedException e) {
+      throw new Error(e);
+    }
+  }
 
-     public void sort() {
-          sort(null);
-     }
+  public void sort() {
+    sort(null);
+  }
 
-     @SuppressWarnings("unchecked")
-     public void sort(Comparator<? super E> c) {
-          Arrays.sort((E[]) array, 0, size, c);
-     }
+  @SuppressWarnings("unchecked")
+  public void sort(Comparator<? super E> c) {
+    Arrays.sort((E[]) array, 0, size, c);
+  }
 
-     public Object[] toArray() {
-          return Arrays.copyOf(array, size);
-     }
+  public Object[] toArray() {
+    return Arrays.copyOf(array, size);
+  }
 
-     @SuppressWarnings("unchecked")
-     public <T> T[] toArray(T[] a) {
-          if (a.length < size) {
-               return (T[]) Arrays.copyOf(array, size, a.getClass());
-          }
+  @SuppressWarnings("unchecked")
+  public <T> T[] toArray(T[] a) {
+    if (a.length < size) {
+      return (T[]) Arrays.copyOf(array, size, a.getClass());
+    }
 
-          System.arraycopy(array, 0, a, 0, size);
+    System.arraycopy(array, 0, a, 0, size);
 
-          if (a.length > size) {
-               a[size] = null;
-          }
-          return a;
-     }
+    if (a.length > size) {
+      a[size] = null;
+    }
+    return a;
+  }
 
-     @Override
-     public Iterator<E> iterator() {
-          return new Iter();
-     }
+  @Override
+  public Iterator<E> iterator() {
+    return new Iter();
+  }
 
-     private class Iter implements Iterator<E> {
+  private class Iter implements Iterator<E> {
 
-          private int now = 0;
+    private int now = 0;
 
-          @Override
-          public boolean hasNext() {
-               return now < size;
-          }
+    @Override
+    public boolean hasNext() {
+      return now < size;
+    }
 
-          @SuppressWarnings("unchecked")
-          @Override
-          public E next() {
-               int cs = now;
-               if (cs >= size) {
-                    throw new NoSuchElementException();
-               }
-               Object[] data = ArrayList.this.array;
-               now = cs + 1;
-               return (E) data[cs];
-          }
+    @SuppressWarnings("unchecked")
+    @Override
+    public E next() {
+      int cs = now;
+      if (cs >= size) {
+        throw new NoSuchElementException();
+      }
+      Object[] data = ArrayList.this.array;
+      now = cs + 1;
+      return (E) data[cs];
+    }
 
-          public void remove() {
-               throw new UnsupportedOperationException();
-          }
-     }
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
 }
